574e183b311b98cc84ed3defd3640c6c18dd455a
IvayloPaskalev <ivaylo.paskalew@gmail.com>
2016-08-14 00:58:02 -0700

    Small fixes

--- 03.jQuery/task-2/task-2.js
+++ 03.jQuery/task-2/task-2.js
@@ -10,21 +10,19 @@ function solve() {
             throw '';
         }
 
-        var $element = $(selector);
-
-        var $listOfButtons = $('.button, .content');
+        var $element = $(selector),
+            $listOfButtons = $('.button, .content');
 
         $element.find('.button')
             .text('hide');
 
         $('.button').on('click', function () {
-            var $this = $(this);
-            var $node = $this;
-
-            var $nextEl = $node
-                .nextUntil('.button')
-                .filter('.content')
-                .first();
+            var $this = $(this),
+                $node = $this,
+                $nextEl = $node
+                    .nextUntil('.button')
+                    .filter('.content')
+                    .first();
 
             if ($nextEl.length === 0) {
                 return;
@@ -33,7 +31,6 @@ function solve() {
             if ($nextEl.css('display') === 'none') {
                 $node.text('hide');
                 $nextEl.css('display', '');
-
             } else {
                 $node.text('show');
                 $nextEl.css('display', 'none');
