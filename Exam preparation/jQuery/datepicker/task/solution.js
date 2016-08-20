f999d849466ea14de0add0b2bc04578e72201bf0
IvayloPaskalev <ivaylo.paskalew@gmail.com>
2016-08-19 20:33:32 -0700

    Added Handlebars task-1

--- Exam preparation/jQuery/datepicker/task/solution.js
+++ Exam preparation/jQuery/datepicker/task/solution.js
@@ -1,8 +1,13 @@
 ﻿function solve() {
     $.fn.datepicker = function () {
+        var $inputDatepicker = $(this);
+        var $datePickerWrapper = $inputDatepicker.parent();
+        $datePickerWrapper.addClass('datepicker-wrapper');
+        $inputDatepicker.addClass('datepicker');
+
         
     };
-};
+}
 
 if(typeof module !== 'undefined') {
     module.exports = solve;
