1ea39f18a496c3ed50e71b3ff39e5e5a34098294
IvayloPaskalev <ivaylo.paskalew@gmail.com>
2016-08-20 10:45:08 -0700

    Added task2

---
+++ 04. Handlebars/task-2/script.js
@@ -0,0 +1,47 @@
+var data = {
+    animals: [
+        {
+            name: 'Lion',
+            url: 'https://susanmcmovies.files.wordpress.com/2014/12/the-lion-king-wallpaper-the-lion-king-2-simbas-pride-4685023-1024-768.jpg'
+        },
+        {
+            name: 'Turtle',
+            url: 'http://www.enkivillage.com/s/upload/images/a231e4349b9e3f28c740d802d4565eaf.jpg'
+        },
+        {
+            name: 'Dog'
+        },
+        {
+            name: 'Cat',
+            url: 'http://i.imgur.com/Ruuef.jpg'
+        },
+        {
+            name: 'Dog Again'
+        }]
+};
+
+gsmTemplateHTML = document.getElementById('superhero-list').innerHTML;
+
+gsmTemplate = Handlebars.compile(gsmTemplateHTML);
+
+document.getElementsByTagName('container')[0].innerHTML = gsmTemplate(data);
+
+function solve() {
+    return function (selector) {
+        var template = '<div id="container">' +
+            '<h1>Animals</h1>' +
+            '<ul class="animals-list">' +
+            '{{#animals}}' +
+            '<li>' +
+            '{{#if url}}' +
+            '<a href="{{url}}">See a {{name}}</a>' +
+            '{{else}}' +
+            '<a href="http://cdn.playbuzz.com/cdn/3170bee8-985c-47bc-bbb5-2bcb41e85fe9/d8aa4750-deef-44ac-83a1-f2b5e6ee029a.jpg">No link for {{name}}, here is Batman!</a>' +
+            '{{/if}}' +
+            '</li>' +
+            '{{/animals}}' +
+            '</ul>' +
+            '</div>';
+        $(selector).html(template);
+    };
+}
